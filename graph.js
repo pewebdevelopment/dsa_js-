b27f83f791bd68c6f756f36a457c9e1797d470be
Umair Syed <itsumairsyed@gmail.com>
2023-03-22 10:03:00 +0500

    graph and algo files are added

---
+++ graph.js
@@ -0,0 +1,14 @@
+/* 
+    #### GRAPH ####
+    It is a tree like data structure. It has two types:
+    1- Undirected
+    2- Directed
+
+
+        A  B  C  D  E
+    A   0  1  1  
+    B
+    C
+    D
+    E
+*/
