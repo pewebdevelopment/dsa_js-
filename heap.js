58aff349db0792877483073f6bb20b216625e328
Umair Syed <itsumairsyed@gmail.com>
2023-03-22 09:49:53 +0500

    heap.js is added

---
+++ heap.js
@@ -0,0 +1,6 @@
+/* 
+    ###### HEAP #####
+    It is a tree based data structure that follows the following two conditions:
+    1- Complete Binary Tree
+    2- One of two (Min or Max heap) types 
+*/
